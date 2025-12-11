15c4ee82dd5c07112b2ac7d900b6f5b214e91dbe
MarineMagic <marine.bocquet@magicmakers.fr>
2025-12-11 12:03:45 +0100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -20,26 +20,10 @@ namespace AutoEquip {
 
     //% block="give %item to %entity and equip in slot %slot"
 export function autoEquip(entity: Entity, item: MinecraftItem, slot: ArmorSlot): void {
-    if (!entity) return;
-
-    // Give item to inventory not sure if needed, need tests
-    //entity.addItem(item, 1);
-
-    // slot selected from dropdown
-    let slotName = "";
-    switch(slot) {
-        case ArmorSlot.Head: slotName = "armor.head"; break;
-        case ArmorSlot.Chest: slotName = "armor.chest"; break;
-        case ArmorSlot.Legs: slotName = "armor.legs"; break;
-        case ArmorSlot.Feet: slotName = "armor.feet"; break;
-        case ArmorSlot.Hand: slotName = "weapon.mainhand"; break;
-    }
-
-    // run the command
-    let temp = `replaceitem entity ${entity.name} slot.${slotName} 1 ${item.id} 1`;
-    commands.run(temp);
+    commands.run(replaceitem entity @s slot.armor.head acacia_slab 1);
 }
 
 }
+
 
 
